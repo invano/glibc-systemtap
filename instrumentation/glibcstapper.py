fa0488fe0628f45d7cb3641c5e917edd0278ac80
Emanuele Cozzi <cozziemanuele@gmail.com>
2018-08-04 12:38:32 +0200

    Parse functions prototypes and store results. Verbosity option

--- instrumentation/glibcstapper.py
+++ instrumentation/glibcstapper.py
@@ -5,6 +5,7 @@ import sys
 
 import argparse
 import configparser
+import logging as log
 
 import pycparser
 
@@ -48,22 +49,38 @@ CPP_OPTS = [
 ERR_CPP = ["strdup"]
 ERR_CPP_OPTS = CPP_OPTS + ["-D_LIBC"]
 
-class FunctionParameter(pycparser.c_ast.NodeVisitor):
+CPROTOS = {}
+class CProto(object):
+    def __init__(self, file, func, realfunc, args):
+        self.file = file
+        self.func = func
+        self.realfunc = realfunc
+        self.args = args
+    def __str__(self):
+        return "File: %s, function: %s, args: %s" % (self.file, self.realfunc, self.args)
 
+class FunctionParameter(pycparser.c_ast.NodeVisitor):
+    def __init__(self, file, func):
+        self.file = file
+        self.func = func
+    
     def visit_FuncDef(self, node):
-        #node.decl.type.args.params
-        print (node.decl.name)
-        for params in (node.decl.type.args.params):
-            print (params.name)
+        name = node.decl.name
+        if self.func[::-1] != name[::-1][:len(self.func)]:
+            log.info("Discarding function: %s" % name)
+            return
+        args = [ params.name for params in (node.decl.type.args.params)]
+        CPROTOS[self.func] = CProto(self.file, self.func, name, args)
+        log.info(CPROTOS[self.func])
 
 def instrument(cfile, func):
-    print ("Analyzing %s" % cfile)
+    log.info("Analyzing %s" % cfile)
     if func not in ERR_CPP:
         ast = pycparser.parse_file(cfile, use_cpp=True, cpp_args=CPP_OPTS)
     else:
         ast = pycparser.parse_file(cfile, use_cpp=True, cpp_args=ERR_CPP_OPTS)
 
-    vf = FunctionParameter()
+    vf = FunctionParameter(cfile, func)
     vf.visit(ast)
     
 # def instrument(cfile, func):
@@ -100,8 +117,16 @@ def main(config):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Libcstapper - systemtap uprobes instrumentation")
     parser.add_argument("-c", "--config", nargs=1, type=str, help="Configuration file with list of functions to instrument")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
     args = parser.parse_args()
+    
+    if args.verbose:
+        log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+    else:
+        log.basicConfig(format="%(levelname)s: %(message)s")
+    
     config_file = args.config[0]
+    log.info("Configuration file: %s" % config_file)
 
     if not config_file:
         parser.print_help()
